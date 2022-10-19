52f71479900c6bbd9b2c2b16c9cd928e074941ac
pavelgre4a <pavelgre4a@gmail.com>
2022-10-19 10:19:07 +0300

    feat: add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import {Header} from "./components/Header/Header";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Sobecity</h1>;
+  return (
+    <div>
+      <Header isLoggedIn={true}></Header>
+      <h1 className="text-3xl font-bold underline">Sobecity</h1>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header/BurgerMenu/BurgerMenu.tsx
@@ -0,0 +1,9 @@
+export function BurgerMenu(props: any) {
+    return (
+        <div className="flex flex-col justify-between gap-[1vw]">
+            <span className="w-[5vw] h-[0.5vw] bg-white"></span>
+            <span className="w-[5vw] h-[0.5vw] bg-white"></span>
+            <span className="w-[5vw] h-[0.5vw] bg-white"></span>
+        </div>
+    );
+}
