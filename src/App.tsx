56609fa0bdc59f4a8562847da3dace64e5949431
Andrew Rafalsky <andraffal@gmail.com>
2022-10-20 12:39:42 +0300

    feat: add default styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">Sobecity</h1>;
+  return (
+    <h1 className="text-3xl font-oswald text-white text- underline">
+      Sobecity
+    </h1>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    colors: {
+      primary: {
+        dark: '#641E1F',
+        darkest: '#461A1B',
+        default: '#7B2224',
+        light: '#AF4141',
+      },
+      secondary: {
+        dark: '#666060',
+        default: '#757171',
+      },
+      black: {
+        dark: '#16171B',
+        defalut: '#212226',
+      },
+      grey: '#505154',
+      white: '#FFFFFF',
+    },
+    fontFamily: {
+      oswald: ['Oswald', 'sans-serif'],
+      firaSans: ['Fira Sans', 'sans-serif'],
+      firaCode: ['Fira Code', 'monospace'],
+    },
+    screens: {
+      sm: { min: '414px' },
+      md: { min: '768px' },
+      lg: { min: '1024px' },
+      xl: { min: '1280px' },
+    },
     extend: {},
   },
   plugins: [],
-}
+};
