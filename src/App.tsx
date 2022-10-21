b7bc057b468e35941aefb8d801a3ce02cc64af5c
Andrew Rafalsky <57481641+Slanx@users.noreply.github.com>
2022-10-21 11:20:05 +0300

    feat: add default styles (#5)
    * feat: add default styles
    
    * fix: fix types/node
    
    * feat:add typography
    
    * feat: add colors for typography

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">Sobecity</h1>;
+  return <h1 className="prose-body-4">Sobecity</h1>;
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,135 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      primary: {
+        dark: '#641E1F',
+        darkest: '#461A1B',
+        default: '#7B2224',
+        light: '#AF4141',
+      },
+      secondary: {
+        dark: '#666060',
+        default: '#757171',
+      },
+      black: {
+        dark: '#16171B',
+        defalut: '#212226',
+      },
+      grey: '#505154',
+      white: '#FFFFFF',
+    },
+    fontFamily: {
+      oswald: 'Oswald, sans-serif',
+      firaSans: 'Fira Sans, sans-serif',
+      firaCode: 'Fira Code, monospace',
+    },
+    screens: {
+      sm: { min: '414px' },
+      md: { min: '768px' },
+      lg: { min: '1024px' },
+      xl: { min: '1280px' },
+    },
+    extend: {
+      typography: (theme) => ({
+        h1: {
+          css: {
+            fontSize: '1.5rem',
+            lineHeight: '120.9%',
+            letterSpacing: '0.05em',
+            fontWeight: theme('fontWeight.medium'),
+            fontFamily: theme('fontFamily.oswald'),
+            color: theme('colors.white'),
+          },
+        },
+        h2: {
+          css: {
+            fontSize: '1.25rem',
+            lineHeight: '120.9%',
+            letterSpacing: '0.05em',
+            fontWeight: theme('fontWeight.medium'),
+            fontFamily: theme('fontFamily.oswald'),
+            color: theme('colors.white'),
+          },
+        },
+        h3: {
+          css: {
+            fontSize: '0.875rem',
+            lineHeight: '120.9%',
+            letterSpacing: '0.05em',
+            fontWeight: theme('fontWeight.medium'),
+            fontFamily: theme('fontFamily.oswald'),
+            color: theme('colors.white'),
+          },
+        },
+        'body-1': {
+          css: {
+            fontSize: '1rem',
+            lineHeight: '142.9%',
+            fontWeight: theme('fontWeight.light'),
+            fontFamily: theme('fontFamily.firaSans'),
+            color: theme('colors.white'),
+          },
+        },
+        'body-2': {
+          css: {
+            fontSize: '0.75rem',
+            lineHeight: '120.9%',
+            fontWeight: theme('fontWeight.light'),
+            fontFamily: theme('fontFamily.firaSans'),
+            color: '#575252',
+          },
+        },
+        'body-3': {
+          css: {
+            fontSize: '0.75rem',
+            lineHeight: '120.9%',
+            fontWeight: theme('fontWeight.normal'),
+            fontFamily: theme('fontFamily.firaSans'),
+            color: 'rgba(255, 255, 255, 0.48)',
+          },
+        },
+        'body-4': {
+          css: {
+            fontSize: '0.875rem',
+            lineHeight: '120.9%',
+            fontWeight: theme('fontWeight.light'),
+            fontFamily: theme('fontFamily.firaCode'),
+            color: '#C0ACAC',
+          },
+        },
+        'button-1': {
+          css: {
+            fontSize: '0.875rem',
+            lineHeight: '120.9%',
+            letterSpacing: '0.05em',
+            fontWeight: theme('fontWeight.normal'),
+            fontFamily: theme('fontFamily.oswald'),
+            textTransform: 'uppercase',
+            color: theme('colors.white'),
+          },
+        },
+        'button-2': {
+          css: {
+            fontSize: '0.875rem',
+            lineHeight: '120.9%',
+            fontWeight: theme('fontWeight.light'),
+            fontFamily: theme('fontFamily.firaSans'),
+            color: theme('colors.white'),
+          },
+        },
+        'button-3': {
+          css: {
+            fontSize: '1.125rem',
+            lineHeight: '120.9%',
+            fontWeight: theme('fontWeight.light'),
+            fontFamily: theme('fontFamily.firaSans'),
+            color: theme('colors.white'),
+          },
+        },
+      }),
+    },
   },
-  plugins: [],
-}
+  plugins: [require('@tailwindcss/typography')],
+};
