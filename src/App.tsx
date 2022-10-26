e77594332c82e4d9799736509014abade2de5cbf
pavelgre4a <pavelgre4a@gmail.com>
2022-10-26 12:48:12 +0300

    feat: add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import Header from "./layouts/Header";
+import Footer from './layouts/Footer';
+import Header from './layouts/Header';
 
 function App() {
   return (
     <div>
       <Header isLoggedIn={true} />
       <h1 className="text-3xl font-bold underline">Sobecity</h1>
+      <Footer />
     </div>
   );
 }
