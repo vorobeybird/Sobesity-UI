11bd5a38b4367296e436c254761350a3000b3764
pavelgre4a <103334312+pavelgre4a@users.noreply.github.com>
2022-10-30 12:39:52 +0300

    feat: add footer (#7)

---
+++ src/layouts/footer/Footer.tsx
@@ -0,0 +1,14 @@
+import languagesLogos from '../../assets/icons/languages.svg';
+
+export function Footer() {
+  return (
+    <div className="w-full sm:h-48 bg-black-dark flex flex-col justify-center items-center sm:p-4">
+      <p className="font-firaSans font-light sm:text-base text-white text-center sm:mb-4">
+        Testing a large number of languages and skills.
+        <br />
+        Most Popular:
+      </p>
+      <img src={languagesLogos} alt="languages logos" className="sm:w-96" />
+    </div>
+  );
+}
