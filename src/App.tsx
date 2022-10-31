2a3a84f1318f3826a38a908621f4faf2bdccfc20
Andrew Rafalsky <57481641+Slanx@users.noreply.github.com>
2022-10-31 11:21:17 +0300

    feat:add gitattribute file (#8)
    Co-authored-by: Varabei Kanstantsin <46844648+vorobeybird@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Header from './layouts/Header';
 function App() {
   return (
     <div>
-      <Header isLoggedIn={true} />
+      <Header isLoggedIn />
       <h1 className="text-3xl font-bold underline">Sobecity</h1>
       <Footer />
     </div>
--- src/components/ProfileButton/ProfileButton.tsx
+++ src/components/ProfileButton/ProfileButton.tsx
@@ -3,11 +3,7 @@ import profileLogo from '../../assets/icons/avatar.svg';
 export function ProfileButton() {
   return (
     <div className="flex">
-      <img
-        src={profileLogo}
-        alt="Profile logo"
-        className="w-[6vw] h-[6vw]"
-      ></img>
+      <img src={profileLogo} alt="Profile logo" className="w-[6vw] h-[6vw]" />
     </div>
   );
 }
