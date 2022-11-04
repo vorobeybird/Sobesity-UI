ab9ef4b6c49caf95c745ff952ccb182947fb88e0
HardKullez <62354609+HardKullez@users.noreply.github.com>
2022-11-04 19:02:50 +0300

    feat: create-api-service (#10)

---
+++ src/services/api/index.ts
@@ -0,0 +1,2 @@
+export { authService } from './auth.service'
+export { questionService } from './question.service'
---
+++ src/services/api/question.service.ts
@@ -0,0 +1,29 @@
+import { apiClient } from "./client";
+import { AxiosResponse } from 'axios'
+
+type QuestionCategory = {
+  id: number,
+  name: string
+}
+
+type Question = {
+  question: string,
+  answers: string[],
+  correctAnswerId: number
+}
+
+interface QuestionService {
+  getAllCategories(): Promise<AxiosResponse<QuestionCategory[]>>,
+  getCategoryQuestions(category: QuestionCategory): Promise<AxiosResponse<Question[]>>
+}
+
+const questionService: QuestionService = {
+  getAllCategories() {
+    return apiClient.get('categories')
+  },
+  getCategoryQuestions(category) {
+    return apiClient.get(`categories/${category.id}`)
+  },
+}
+
+export { questionService }
