9740a91ade7007b65d5680767dab9d7c98f8d8b3
HardKullez <darkangel606@mail.ru>
2022-12-05 23:43:54 +0300

    refactor: fix imports & remove unnecessary from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import InfoCards from 'components/ui/info-cards';
 import { Footer } from 'layouts/footer/Footer';
-import Header from './layouts/header';
+import Header from './layouts/Header';
 
 function App() {
   return (
     <div>
       <Header isLoggedIn />
       <h1 className="body-4">Sobecity</h1>
-      <InfoCards />
       <Footer />
     </div>
   );
