03a9cd8fa866895c0c6f6a7029be9829b5f25240
unknown <Administrator@INV-771PL-L.godeltech.com>
2022-12-09 15:43:18 +0100

    SOB-42 fix commit

--- src/App.tsx
+++ src/App.tsx
@@ -9,25 +9,49 @@ function App() {
       <Header isLoggedIn />
       <h1 className="body-4">Sobecity</h1>
       <div className="flex flex-col align-middle justify-center">
-        <Carousel interval={1500000} transitionTime={500}>
-          <div className="text-container">
-            <h1>Slide 0</h1>
+        <Carousel interval={10000} transitionTime={500}>
+          <div className="bg-black-dark p-16 ">
+            <h1 className="text-center text-secondary-light text-2xl">
+              Slide 0
+            </h1>
           </div>
-          <div className="text-container">
-            <h1>Slide 1</h1>
+          <div className="bg-black-dark p-16 ">
+            <h1 className="text-center text-secondary-light text-2xl">
+              Slide 1
+            </h1>
           </div>
-          <div className="text-container">
-            <h1>Slide 2</h1>
+          <div className="bg-black-dark p-16 ">
+            <h1 className="text-center text-secondary-light text-2xl">
+              Slide 2
+            </h1>
           </div>
-          <div className="text-container">
-            <h1>Slide 3</h1>
+          <div className="bg-black-dark p-16 ">
+            <h1 className="text-center text-secondary-light text-2xl">
+              Slide 3
+            </h1>
           </div>
-          <div className="text-container">
-            <h1>Slide 4</h1>
+        </Carousel>
+        <Carousel interval={1000} transitionTime={500}>
+          <div className="image-container">
+            <img
+              src="https://images.unsplash.com/photo-1446729444801-31245ddba81a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+              alt="slide"
+            />
+          </div>
+          <div className="image-container">
+            <img
+              src="https://images.unsplash.com/photo-1422207258071-70754198c4a2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=741&q=80"
+              alt="slide"
+            />
+          </div>
+          <div className="image-container">
+            <img
+              src="https://images.unsplash.com/photo-1473864803180-ca1b3d93c9a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=749&q=80"
+              alt="slide"
+            />
           </div>
         </Carousel>
       </div>
-
       <InfoCards />
       <Footer />
     </div>
