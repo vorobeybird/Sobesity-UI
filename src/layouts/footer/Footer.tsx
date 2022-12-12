ad2aee193c3985c6475c67cab5527f0e421624cf
Kanstantsin Varabei <vorobeybird@gmail.com>
2022-12-12 16:04:46 +0100

    NEW YEAR COMMIT

--- src/layouts/footer/Footer.tsx
+++ src/layouts/footer/Footer.tsx
@@ -2,7 +2,7 @@ import languagesLogos from '../../assets/icons/languages.svg';
 
 export function Footer() {
   return (
-    <div className="w-full sm:h-48 bg-black-dark flex flex-col justify-center items-center sm:p-4">
+    <div className="w-full sm:h-48 bg-primary bg-cover flex flex-col justify-center items-center sm:p-4">
       <p className="font-firaSans font-light sm:text-base text-white text-center sm:mb-4">
         Testing a large number of languages and skills.
         <br />
