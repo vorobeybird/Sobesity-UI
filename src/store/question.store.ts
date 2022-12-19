72ab7183ba34b017a63717c6c58d2c3e10df08d7
HardKullez <darkangel606@mail.ru>
2022-12-19 21:09:37 +0300

    feat: add basic reducer as example

--- src/store/question.store.ts
+++ src/store/question.store.ts
@@ -1,16 +1,62 @@
-import { Action } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { questionService } from '../services/api/question.service';
 
-// const initialState = {
-//   questions: [],
-// };
+export type QuestionCategory = {
+  id: number;
+  name: string;
+  questions: Question[];
+};
 
-export function questionReducer(state: any, action: Action) {
-  if (action.type === 'fetchQuestions') {
-    return {
-      ...state,
-      value: state.questions || [],
-    };
-  }
+export type Question = {
+  question: string;
+  answers: string[];
+  correctAnswerId: number;
+};
 
-  return null;
+export interface QuestionsState {
+  entities: QuestionCategory[] | [];
+  loading: 'idle' | 'pending' | 'succeeded' | 'failed';
 }
+
+const initialState: QuestionsState = {
+  entities: [],
+  loading: 'idle',
+};
+
+export const getCategoryQuestions = createAsyncThunk(
+  'questions/get',
+  async (category: QuestionCategory) => {
+    const { data } = await questionService.getCategoryQuestions(category);
+
+    return data;
+  },
+);
+
+export const getAllCategories = createAsyncThunk(
+  'questions/getAll',
+  async () => {
+    const { data } = await questionService.getAllCategories();
+
+    return data;
+  },
+);
+
+const questionSlice = createSlice({
+  name: 'questions',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getAllCategories.fulfilled, (state, { payload }) => {
+      // just example not dealing with the types or assigning
+      // eslint-disable-next-line no-param-reassign
+      state.entities[1].questions = payload[1].questions;
+    });
+    builder.addCase(getCategoryQuestions.fulfilled, (state, { payload }) => {
+      // just example not dealing with the types or assigning
+      // eslint-disable-next-line no-param-reassign
+      state.entities[1].questions = payload;
+    });
+  },
+});
+
+export const { reducer: questionReducer } = questionSlice;
