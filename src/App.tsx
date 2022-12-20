a94b0a6329d80582b34c7e51b3ffa869cec3802e
Kanstantsin Varabei <vorobeybird@gmail.com>
2022-12-20 11:33:14 +0100

    SOB-48 add mobile design

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,17 @@
-// import InfoCards from 'components/ui/info-cards';
-
-// import Carousel from 'components/carousel';
 import { CreateAccount } from 'components/ui/info-components/create-account';
 import { Footer } from 'layouts/footer/Footer';
 import Header from './layouts/Header';
 
 function App() {
   return (
-    <div className="grid grid-rows-6 bg-black-dark ">
+    <div className=" bg-black-dark ">
       <Header isLoggedIn />
       {/* <h1 className="body-4">Sobecity</h1> */}
-      <div className="row-span-4 flex flex-col items-center px-[60px]">
-        <div className="w-full ">
+      <div className="flex flex-col w-min-[370px] w-max-[1280px] items-center px-[22px] lg:px-[60px]">
+        <div className="w-full py-[20px]">
           <CreateAccount />
         </div>
       </div>
-      {/* <div className="flex flex-col align-middle justify-center">
-        <Carousel interval={10000}>
-          <div className="bg-black-dark p-16 ">
-            <h1 className="text-center text-secondary-light text-2xl">
-              Slide 0
-            </h1>
-          </div>
-          <div className="bg-black-dark p-16 ">
-            <h1 className="text-center text-secondary-light text-2xl">
-              Slide 1
-            </h1>
-          </div>
-          <div className="bg-black-dark p-16 ">
-            <h1 className="text-center text-secondary-light text-2xl">
-              Slide 2
-            </h1>
-          </div>
-          <div className="bg-black-dark p-16 ">
-            <h1 className="text-center text-secondary-light text-2xl">
-              Slide 3
-            </h1>
-          </div>
-        </Carousel>
-        <Carousel>
-          <div>
-            <img
-              className="w-full h-60 object-cover pointer-events-none"
-              src="https://images.unsplash.com/photo-1446729444801-31245ddba81a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
-              alt="slide"
-            />
-          </div>
-          <div>
-            <img
-              className="w-full h-60 object-cover pointer-events-none"
-              src="https://images.unsplash.com/photo-1422207258071-70754198c4a2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=741&q=80"
-              alt="slide"
-            />
-          </div>
-          <div>
-            <img
-              className="w-full h-60 object-cover pointer-events-none"
-              src="https://images.unsplash.com/photo-1473864803180-ca1b3d93c9a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=749&q=80"
-              alt="slide"
-            />
-          </div>
-        </Carousel>
-        <InfoCards />
-      </div> */}
       <Footer />
     </div>
   );
--- src/components/ui/info-components/create-account/create-account.tsx
+++ src/components/ui/info-components/create-account/create-account.tsx
@@ -3,18 +3,24 @@ import LaptopImage from '../../../../assets/images/laptop.svg';
 
 const CreateAccount: FC<any> = () => {
   return (
-    <div className="flex justify-between h-[576px]  overflow-hidden text-white bg-black-light shadow-xl ">
-      <div className="flex-1 h-full relative">
-        <div className="absolute flex justify-center items-center bg-secondary-dark top-[-110px] left-[-150px] w-[800px] h-[800px] rounded-full ">
-          <div className="bg-secondary-light w-[518px] h-[518px] rounded-full" />
-        </div>
-        <div className="absolute top-[130px]">
-          <img src={LaptopImage} alt="laptop" />
+    <div className="grid grid-cols-3 overflow-hidden bg-black-light shadow-xl rounded-lg ">
+      <div className="grid col-span-2  grid-cols-[minmax(0,0)_240px] grid-rows-[minmax(0,0)_235px] items-center justify-items-center lg:h-[576px]">
+        <div className="col-[1_/_span_2]  row-[1_/_span_2] -ml-[50px] min-h-[300px] min-w-[300px] bg-secondary-dark lg:w-[800px] lg:h-[800px] rounded-full " />
+        <div className="col-[1_/_span_2] row-[1_/_span_2] bg-secondary-light min-w-[194px] min-h-[194px] lg:w-[518px] lg:h-[518px] rounded-full" />
+        <div className="col-[1_/_span_2] row-[1_/_span_2]">
+          <img
+            className="h-[123px] lg:h-[330px] max-w-none"
+            src={LaptopImage}
+            alt="laptop"
+          />
         </div>
       </div>
-      <div className="flex-1 flex flex-col items-center">
-        <div className="h-1 p-[60px]">Create an account</div>
-        <div className="w-[80%]">
+      <div className="flex flex-col justify-end lg:justify-center">
+        <div className="h2 text-right mr-[20px] ">
+          <p className="pb-[10px]">Create</p>
+          <p className="pb-[20px]">an account</p>
+        </div>
+        <div className="hidden lg:block">
           Platea adipiscing in dolor ornare ut. Dui ex. Morbi sodales et. Mollis
           amet, quis, odio. Dictum faucibus. Aenean in amet, platea sed non ut.
           Tempus nunc dictum. In ipsum tempus non ultricies. Vestibulum ornare
