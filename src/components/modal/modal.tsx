3a5ecef197f88edd3adc919a4c8fcfc1d141e2c4
Andrew Rafalsky <andraffal@gmail.com>
2023-01-05 12:21:43 +0300

    feat:add story and animation

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 import { CSSTransition } from 'react-transition-group';
 import Cross from 'components/shared/icons/Cross';
@@ -6,41 +6,39 @@ import { ModalProps } from './modal.types';
 
 const modalRoot = document.getElementById('modal-root') as HTMLElement;
 
-export const Modal = ({ visible, onClose, children }: ModalProps) => {
-  const handleKeyDown = useCallback(
-    (e: KeyboardEvent) => {
-      if (e.key === 'Escape') onClose();
-    },
-    [onClose],
-  );
+export const Modal = ({ isOpen, onClose, children }: ModalProps) => {
+  const nodeRef = useRef(null);
 
   useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === 'Escape') onClose();
+    };
+
     document.body.addEventListener('keydown', handleKeyDown);
 
     return () => {
       document.body.removeEventListener('keydown', handleKeyDown);
     };
-  }, [handleKeyDown]);
+  }, [onClose]);
 
   return createPortal(
     <CSSTransition
-      in={visible}
-      timeout={150}
+      in={isOpen}
+      timeout={200}
       classNames={{
         enter: 'opacity-0',
-        enterActive: 'opacity-100 easy-out duration-150',
-        enterDone: 'opacity-100',
-        exit: 'opacity-100',
-        exitActive: 'opacity-50 easy-out duration-150',
-        exitDone: 'opacity-0',
+        enterActive: 'opacity-100 duration-200',
+        exitActive: 'opacity-0 duration-200',
       }}
       mountOnEnter
       unmountOnExit
+      nodeRef={nodeRef}
     >
       <div
         onMouseDown={onClose}
         aria-hidden
         className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-30 backdrop-blur-sm"
+        ref={nodeRef}
       >
         <div
           aria-hidden
@@ -49,10 +47,10 @@ export const Modal = ({ visible, onClose, children }: ModalProps) => {
         >
           <button
             type="button"
-            className="absolute right-[3%] translate-y-full"
+            className="absolute right-[2%] translate-y-1/2"
             onClick={onClose}
           >
-            <Cross className="fill-secondary-dark h-[1rem] w-[1rem] lg:h-[1rem] lg:w-[1rem]" />
+            <Cross className="fill-secondary-dark h-[0.9rem] w-[0.9rem] lg:h-[1rem] lg:w-[1rem]" />
           </button>
           {children}
         </div>
