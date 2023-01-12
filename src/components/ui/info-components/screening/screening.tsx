dd114421999ebbe46975ae5ca4a947871ae41522
pavelgre4a <pavelgre4a@gmail.com>
2023-01-12 22:15:57 +0300

    SOB-61 fix: change classnames after review

--- src/components/ui/info-components/screening/screening.tsx
+++ src/components/ui/info-components/screening/screening.tsx
@@ -1,4 +1,3 @@
-import classNames from 'classnames';
 import { useTranslation } from 'react-i18next';
 import Someone from '../../../../assets/images/Someone.svg';
 
@@ -6,37 +5,15 @@ export function Screening() {
   const { t } = useTranslation();
 
   return (
-    <div
-      className={classNames('sm:hidden md:hidden', 'xl:flex w-full h-[600px]')}
-    >
-      <div
-        className={classNames(
-          'flex basis-2/4 items-center justify-center bg-black-light',
-        )}
-      >
-        <img
-          src={Someone}
-          alt="Someone on the monitor"
-          className={classNames('my-7 mx-4')}
-        />
+    <div className="sm:hidden md:hidden xl:flex w-full h-[600px]">
+      <div className="flex basis-2/4 items-center justify-center bg-black-light">
+        <img src={Someone} alt="Someone on the monitor" className="my-7 mx-4" />
       </div>
-      <div
-        className={classNames(
-          'flex basis-2/4 bg-[#5C1416] items-center justify-center flex-col',
-        )}
-      >
-        <div
-          className={classNames(
-            'font-oswald text-xl text-white font-medium uppercase mb-16',
-          )}
-        >
+      <div className="flex basis-2/4 bg-[#5C1416] items-center justify-center flex-col">
+        <div className="font-oswald text-xl text-white font-medium uppercase mb-16">
           {t('PAGES.HOME.SCREENING_SECTION.TITLE')}
         </div>
-        <div
-          className={classNames(
-            'text-center font-firaSans text-xl text-white font-normal mx-24',
-          )}
-        >
+        <div className="text-center font-firaSans text-xl text-white font-normal mx-24">
           {t('PAGES.HOME.SCREENING_SECTION.DESCRIPTION')}
         </div>
       </div>
