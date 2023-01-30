bd87fe2e040edf058d9453e926ae9d8e19dc1654
Andrew Rafalsky <57481641+Slanx@users.noreply.github.com>
2023-01-30 13:55:38 +0300

    Sob 47 create customizable modal component (#26)
    * feat:add custom modal
    
    * feat:add story and animation
    
    * refactor: delete unnecessary
    
    * fix: fix path to the icon
    
    * Sob 51 landing find interviewer component (#27)
    
    * feat: landing create find interviewers component
    
    * feat: add translation
    
    * SOB-51 fix bg colors
    
    Co-authored-by: Kanstantsin Varabei <vorobeybird@gmail.com>
    
    * Sob 11 add home page (#28)
    
    * add basic landing comp
    
    * SOB-11 fix layout
    
    * SOB-11 add mobile landing
    
    * SOB-11 refactor layout
    
    * SOB-11 add translation to home
    
    * refactor: remove gitkeep
    
    * sob-11 move animation to tw config
    
    Co-authored-by: HardKullez <darkangel606@mail.ru>
    
    * SOB-61 feat: landing  create screening component (#29)
    
    * SOB-61 feat: landing  create screening component
    
    * SOB-61 fix: change classnames after review
    
    * SOB-61 fix: add screening component to home page
    
    * SOB-61 fix dependencies
    
    * SOB-61 add: add shades of red to config
    
    * SOB-61 fix: change styles, update colors in config
    
    Co-authored-by: Kanstantsin Varabei <vorobeybird@gmail.com>
    
    * Update README.md (#30)
    
    * feat:add custom modal
    
    * feat:add story and animation
    
    * refactor: delete unnecessary
    
    * fix: fix path to the icon
    
    * SOB-47 add focus trap
    
    ---------
    
    Co-authored-by: pavelgre4a <103334312+pavelgre4a@users.noreply.github.com>
    Co-authored-by: Kanstantsin Varabei <vorobeybird@gmail.com>
    Co-authored-by: Varabei Kanstantsin <46844648+vorobeybird@users.noreply.github.com>
    Co-authored-by: HardKullez <darkangel606@mail.ru>

---
+++ src/components/modal/modal.tsx
@@ -0,0 +1,64 @@
+import { useEffect, useRef } from 'react';
+import { createPortal } from 'react-dom';
+import { CSSTransition } from 'react-transition-group';
+import { Cross } from 'components/shared/icons';
+import FocusTrap from 'focus-trap-react';
+import { ModalProps } from './modal.types';
+
+const modalRoot = document.getElementById('modal-root') as HTMLElement;
+
+export const Modal = ({ isOpen, onClose, children }: ModalProps) => {
+  const nodeRef = useRef(null);
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === 'Escape') onClose();
+    };
+
+    document.body.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.body.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [onClose]);
+
+  return createPortal(
+    <CSSTransition
+      in={isOpen}
+      timeout={200}
+      classNames={{
+        enter: 'opacity-0',
+        enterActive: 'opacity-100 duration-200',
+        exitActive: 'opacity-0 duration-200',
+      }}
+      mountOnEnter
+      unmountOnExit
+      nodeRef={nodeRef}
+    >
+      <div
+        onMouseDown={onClose}
+        aria-hidden
+        className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-30 backdrop-blur-sm"
+        ref={nodeRef}
+      >
+        <FocusTrap>
+          <div
+            aria-hidden
+            onMouseDown={(e) => e.stopPropagation()}
+            className="relative"
+          >
+            <button
+              type="button"
+              className="absolute right-[2%] translate-y-1/2"
+              onClick={onClose}
+            >
+              <Cross className="fill-secondary-dark h-[0.9rem] m-2 w-[0.9rem] lg:h-4 lg:w-4" />
+            </button>
+            {children}
+          </div>
+        </FocusTrap>
+      </div>
+    </CSSTransition>,
+    modalRoot,
+  );
+};
--- src/components/shared/icons/index.ts
+++ src/components/shared/icons/index.ts
@@ -4,3 +4,4 @@ export { default as Facebook } from './Facebook';
 export { default as Google } from './Google';
 export { default as Languages } from './Languages';
 export { default as Lock } from './Lock';
+export { default as Cross } from './Cross';
