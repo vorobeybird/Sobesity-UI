d2434ba578f84483980f901f3479556094b67d4a
Kanstantsin Varabei <k.varabei@godeltech.com>
2023-01-31 11:58:22 +0000

    Deploy domain configuration

--- deploy/sh/deploy-prod.sh
+++ deploy/sh/deploy-prod.sh
@@ -4,6 +4,6 @@ set -xe
 image=$( cat image )
 
 sh ${SHARED_SCRIPTS_DIR}/sh/deploy-argocd.sh \
-  -p "applications/prod/lnd/sobesity/sobesity.yaml" \
+  -p "applications/prod/ta/sobesity/sobesity.yaml" \
   -k "deploy/k8s/prod" \
   -i $image
