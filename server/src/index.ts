440aeb73bde68522b138f96ad97bc26c50c0c21d
Kanstantsin Varabei <vorobeybird@gmail.com>
2023-03-11 17:16:30 +0100

    dockerize monorepo

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,13 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+
 import express from 'express';
 import { json } from 'body-parser';
 import 'express-async-errors';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import path from 'path';
+
 
 import { currentUserRouter } from './routes/current-user';
 import { signInRouter } from './routes/signin';
@@ -12,12 +16,13 @@ import { signUpRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 
-const app = express();
-app.use(cors());
-console.log(path.join(__dirname, '/client'));
 
+const app = express();
+
+app.use(cors());
 
 app.use(express.json());
+
 app.use(currentUserRouter);
 app.use(signInRouter);
 app.use(signOutRouter);
@@ -32,7 +37,6 @@ app.use(express.static(path.join(__dirname, '../../client/build'), {
 }))
 
 app.get('*', (req, res) => {
-    console.log(path.join(__dirname, '../../client/build/index.html'));
     res.status(200).sendFile(path.join(__dirname, '../../client/build/index.html'));
 });
 
@@ -40,15 +44,18 @@ app.use(errorHandler);
 
 const start = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/auth');
+        console.log(`${process.env.DB_URL}:${process.env.DB_PORT}/auth`)
+        await mongoose.connect(`${process.env.DB_URL}:${process.env.DB_PORT}/auth`);
         console.log('Connected to mongodb, port 27017')
     } catch (err) {
         console.error(err);
     }
 }
 
-app.listen(5150, () => {
-    console.log('Listening on port 5150!!!');
+const PORT = process.env.PORT || 5150;
+
+app.listen(PORT, () => {
+    console.log('Listening on port: ', PORT);
 });
 
 
--- server/src/middlewares/error-handler.ts
+++ server/src/middlewares/error-handler.ts
@@ -10,10 +10,10 @@ export const errorHandler = (
     if (err instanceof CustomError) {
         return res.status(err.statusCode).send({ errors: err.serializeErrors() });
     }
-
+    //debugging
     res.status(400).send({
         errors: [
-            { message: 'Something went wrong' }
+            { message: `Something went wrong, ${err.message}` }
         ]
     });
 
